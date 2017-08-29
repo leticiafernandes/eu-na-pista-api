9e897cfefaa1a9af9716a2d28839fb42709bdfcb
Leticia <leticia@macbook-pro-60.corp.globoi.com>
2017-08-29 17:58:52 -0300

    stylling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post '/events' => 'events#create', defaults: { format: 'json' }
+  post '/events' => 'events#create', defaults: { format: 'js' }
   resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
