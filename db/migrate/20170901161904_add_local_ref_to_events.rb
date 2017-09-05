537d1a2f03a6692167ff419cd3d93bfe5c22e06a
Leticia <leticia@macbook-pro-60.corp.globoi.com>
2017-09-05 10:01:36 -0300

    removendo unused

--- db/migrate/20170901161904_add_local_ref_to_events.rb
+++
@@ -1,5 +0,0 @@
-class AddLocalRefToEvents < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :events, :local, foreign_key: true
-  end
-end
--- db/migrate/20170901165154_remove_local_from_events.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLocalFromEvents < ActiveRecord::Migration[5.1]
-  def change
-    remove_reference :events, :local, foreign_key: true
-  end
-end
