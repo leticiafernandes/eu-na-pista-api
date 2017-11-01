eb96d35d4ac0619d98823751ab9569ca6d7acb6c
Leticia <leticia@macbook-pro-60.corp.globoi.com>
2017-11-01 13:02:41 -0200

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+  
+  before_action :authenticate_user_from_token!
   before_action :authenticate_user!
 
   def index
   	
+  end
+
+  private
+  
+  def authenticate_user_from_token!
+    user_email = params[:user_email].presence
+    user       = user_email && User.find_by_email(user_email)
+
+    # Notice how we use Devise.secure_compare to compare the token
+    # in the database with the token given in the params, mitigating
+    # timing attacks.
+    if user && Devise.secure_compare(user.authentication_token, params[:user_token])
+      sign_in user, store: false
+    end
   end
 end
