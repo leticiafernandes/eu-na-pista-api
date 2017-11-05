367a31a87edacbb1cb9a805cc1ff6891928676fa
Leticia <leticia@MacBook-Pro-60.local>
2017-11-05 20:03:46 -0200

    login ok

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-	skip_before_action :verify_authenticity_token
+	skip_before_action :verify_authenticity_token, raise: false
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   # GET /events
